533cadae9d14586ddd1f84ec81a07a1c52ff5ab7
João Vítor <joaovsoutosantos@gmail.com>
2021-12-13 22:18:51 -0300

    create landing page response type and turn logo dynamic

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+
+import type { LogoProps } from 'types/api'
+
 import * as S from './styles'
 
-const Logo = () => <S.LogoWrapper src="/img/logo.svg" alt="React Avançado" />
+type Props = LogoProps
+
+const Logo = ({ alternativeText, url }: Props) => (
+  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+)
 
 export default Logo
