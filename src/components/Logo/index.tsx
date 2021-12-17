23cc2ed9d7bc1860a2b4f1a2232fafebb942273a
João Vítor <joaovsoutosantos@gmail.com>
2021-12-17 17:52:05 -0300

    create env variables for graphql and images host

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import type { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 
 import * as S from './styles'
 
 type Props = LogoProps
 
 const Logo = ({ alternativeText, url }: Props) => (
-  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
 export default Logo
